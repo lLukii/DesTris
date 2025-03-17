6342e1545bd2ff09fcb103a5cabc551e1af0eadf
Lucas Jiao <105329087+lLukii@users.noreply.github.com>
2025-03-16 22:26:23 -0400

    updates

--- tetris.js
+++ tetris.js
@@ -7,13 +7,34 @@ var tileTypes = [[[0,0],[0,1],[1,0],[1,1]],
     [[0,0],[0,1],[0,2],[1,2]],
     [[0,0],[0,1],[0,-1],[1,0]]]
 
-var colorPalette = ['#00ffff', '#ffff00', '#800080', '#00ff00', '#ff0000', '#0000ff', '#ff7f00']
+var colorPalette = ['#c74440', '#2d70b3', '#388c46', '#6042a6', '#fa7e19', '#000000']
 
 var activeTiles = []
 var gameBoard = []
 var piece_num = 0
 var gameState = 'generate'
 var gameOn = true
+
+calculator.setExpression({
+    id: 'b1',
+    latex: 'y=0\\{0\\le x \\le 10\\}',
+    color: '#000000'
+})
+calculator.setExpression({
+    id: 'b2',
+    latex: 'y=24\\{0\\le x \\le 10\\}',
+    color: '#000000'
+})
+calculator.setExpression({
+    id: 'b3',
+    latex: 'x=0\\{0\\le y \\le 24\\}',
+    color: '#000000'
+})
+calculator.setExpression({
+    id: '41',
+    latex: 'x=10\\{0\\le y \\le 24\\}',
+    color: '#000000'
+})
 
 
 for(var i = 0; i < 24; i++){
@@ -26,9 +47,12 @@ for(var i = 0; i < 24; i++){
 function generatePiece(){
     var piece_type = tileTypes[Math.floor(Math.random() * 4)];
     var init_y = 23, init_x = 4;
-    var color = colorPalette[Math.floor(Math.random() * 6)];
+    var color = colorPalette[Math.floor(Math.random() * 5)];
     for(var i = 0; i < 4; i++){
         activeTiles[i] = [init_x - piece_type[i][1], init_y - piece_type[i][0], [piece_num, i], color];
+    }
+    if(checkForCollision()){
+        window.close()
     }
 }
 
@@ -58,8 +82,19 @@ function displayTiles(){
     }
 }
 
+function checkForCollision(){
+    for(var tile of activeTiles){
+        var x = tile[0], y = tile[1], identity = tile[2][0];
+        if(y == 0 || (gameBoard[y-1][x] != 0 && gameBoard[y-1][x][0][0] != identity)) return true;
+    }
+    return false;
+}
 
-document.addEventListener('DOMContentLoaded', function(){
+function checkForLineClear(){
+
+}
+
+function gameLoop(){
     if(gameState == 'generate'){
         piece_num++;
         activeTiles = []
@@ -68,24 +103,32 @@ document.addEventListener('DOMContentLoaded', function(){
         gameState = 'Falling'; 
     }
     if(gameState == 'Falling'){
-
-        document.addEventListener('keydown', function(event) {
-            if(event.keyCode == 40){
-                // down arrow
-                event.preventDefault();
-                shiftPiece([0, -1])
-                displayTiles()
-            }
-            if(event.keyCode == 37){
-                event.preventDefault();
-                shiftPiece([-1, 0])
-                displayTiles()
-            }
-            if(event.keyCode == 39){
-                event.preventDefault();
-                shiftPiece([1, 0])
-                displayTiles()
-            }
-        });
+        if(checkForCollision()){
+            gameState = 'generate'
+            checkForLineClear()
+        }
     }
+    requestAnimationFrame(gameLoop);
+}
+
+document.addEventListener('keydown', function(event) {
+    if(event.keyCode == 40){
+        event.preventDefault();
+        shiftPiece([0, -1])
+        displayTiles()
+    }
+    if(event.keyCode == 37){
+        event.preventDefault();
+        shiftPiece([-1, 0])
+        displayTiles()
+    }
+    if(event.keyCode == 39){
+        event.preventDefault();
+        shiftPiece([1, 0])
+        displayTiles()
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+    gameLoop();
 })
